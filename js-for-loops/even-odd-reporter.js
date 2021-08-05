dfea54ba2441352e987a4271f0053d94f095e493
JEBesteman <86465468+JEBesteman@users.noreply.github.com>
2021-08-05 12:09:29 +0200

    Update even-odd-reporter.js

--- js-for-loops/even-odd-reporter.js
+++ js-for-loops/even-odd-reporter.js
@@ -1,5 +1,5 @@
 for (let number = 0; number <= 20; number++) {
-  if (number % 2 == 0) {
+  if (number % 2 === 0) {
     console.log(`${number} is even`);
   } else {
     console.log(`${number} is odd`);
